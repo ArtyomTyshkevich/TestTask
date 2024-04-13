a995ee78e8c9d64db3bd0fdc4d4f1abc67da9637
Tyshkevich Artyom <frod_1999@mail.ru>
2024-04-13 15:02:31 +0300

    Add project files.

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,29 @@
+﻿using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _db;
+        public OrderService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public Task<Order> GetOrder()
+        {
+           return Task.FromResult( _db.Orders
+               .OrderByDescending(x => x.Price)
+               .FirstOrDefault());
+        }
+
+        public Task<List<Order>> GetOrders()
+        {
+            return Task.FromResult(_db.Orders
+                .Where(x=> x.Quantity>10)
+                .ToList());
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,32 @@
+﻿using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _db;
+        public UserService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public Task<User> GetUser()
+        {
+            User test = _db.Users
+                .OrderByDescending(x => x.Orders.Count)
+                .FirstOrDefault() ;
+            return Task.FromResult(
+                test);
+        }
+
+        public Task<List<User>> GetUsers()
+        {
+            return Task.FromResult(
+                _db.Users
+                .Where(x=> x.Status == Enums.UserStatus.Inactive)
+                .ToList()
+                );
+        }
+    }
+}
